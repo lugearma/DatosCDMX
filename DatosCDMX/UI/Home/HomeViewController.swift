fc4966822315ec3e2c6b67a3d29c525b340364ed
Luis Arias <lugearma@gmail.com>
2019-01-16 13:59:05 -0600

    Reidentated code

---
+++ DatosCDMX/UI/Home/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  DatosCDMX
+//
+//  Created by Luis Arias on 1/16/19.
+//  Copyright © 2019 Luis Arias. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  
+}
