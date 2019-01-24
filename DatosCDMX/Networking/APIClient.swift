42a79f9a129636d30f1d9bf4a396907c3f263827
Luis Arias <lugearma@gmail.com>
2019-01-24 12:24:46 -0600

    Remove fake request

--- DatosCDMX/Networking/APIClient.swift
+++ DatosCDMX/Networking/APIClient.swift
@@ -18,11 +18,10 @@ protocol ApiClientProtocol {
 
 extension ApiClientProtocol {
   func defaultRequest<T: Codable>(_ urlRequest: ApiClientRouter, _ completion: @escaping (Result<T>) -> Void) {
-    /*guard let request = try? urlRequest.asURLRequest() else {
+    guard let request = try? urlRequest.asURLRequest() else {
       completion(Result { throw ApiClientError.unknown })
       return
-    }*/
-    let request = URLRequest(url: URL(string: "https://datos.cdmx.gob.mx/api/datasets/1.0/search/?rows=0&facet=theme&timezone=America%2FMexico_City")!)
+    }
     
     let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
       do {
